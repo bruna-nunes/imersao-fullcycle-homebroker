48c26fe430a88cbf1e2d2887c6a7c66edcccbbdf
Bruna Nunes <brugnunes@gmail.com>
2025-02-25 19:02:27 -0300

    feat: add wallets module with nest g resource

---
+++ src/wallets/wallets.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Wallet } from './entities/wallet.entity';
+
+@Injectable()
+export class WalletsService {
+  constructor(@InjectModel(Wallet.name) private walletSchema: Model<Wallet>) {}
+  
+  create(createWalletDto: CreateWalletDto) {
+    return this.walletSchema.create(createWalletDto)
+  }
+
+  findAll() {
+    return this.walletSchema.find()
+  }
+
+  findOne(id: string) {
+    return this.walletSchema.findById(id);
+  }
+}

627ca4fd2aa279fa6b3a84bafedcdacb1509c931
Bruna Nunes <brugnunes@gmail.com>
2025-02-26 18:16:44 -0300

    feat: add WalletAsset collection and configurations

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -3,13 +3,18 @@ import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Wallet, WalletSchema } from './entities/wallet.entity';
+import { WalletAsset, WalletAssetSchema } from './entities/wallet-asset.entity';
 
 @Module({
   imports: [
       MongooseModule.forFeature([ // especificar colections que serao utilizada
         {
           name: Wallet.name,
-          schema: WalletSchema
+          schema: WalletSchema,
+        },
+        {
+          name: WalletAsset.name,
+          schema: WalletAssetSchema,
         },
       ]),
     ],
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -3,10 +3,14 @@ import { CreateWalletDto } from './dto/create-wallet.dto';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Wallet } from './entities/wallet.entity';
+import { WalletAsset } from './entities/wallet-asset.entity';
 
 @Injectable()
 export class WalletsService {
-  constructor(@InjectModel(Wallet.name) private walletSchema: Model<Wallet>) {}
+  constructor(
+    @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name) private walletAssetSchema: Model<WalletAsset>
+  ) {}
   
   create(createWalletDto: CreateWalletDto) {
     return this.walletSchema.create(createWalletDto)
@@ -18,5 +22,17 @@ export class WalletsService {
 
   findOne(id: string) {
     return this.walletSchema.findById(id);
+  }
+
+  createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number
+  }) {
+    return this.walletAssetSchema.create({
+      wallet: data.walletId,
+      asset: data.assetId,
+      shares: data.shares,
+    })
   }
 }

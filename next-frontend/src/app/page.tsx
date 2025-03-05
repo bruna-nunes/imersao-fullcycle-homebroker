70d744ba6e0fbf4d36082a50ba054abb78759ce7
Bruna Nunes <brugnunes@gmail.com>
2025-03-05 19:52:26 -0300

    feat: add nextjs base and initial wallets list

---
+++ next-frontend/src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Button, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
+
+export default function MyWalleList() {
+  return (
+    <div className="flex flex-col space-y-5 flex-grow">
+      <article className="format">
+        <h1>Minha carteira</h1>
+      </article>
+      <div className="overflow-x-auto w-full">
+        <Table className="w-full max-w-full table-fixed">
+          <TableHead>
+            <TableHeadCell>Ativo</TableHeadCell>
+            <TableHeadCell>Cotação</TableHeadCell>
+            <TableHeadCell>Quantidade</TableHeadCell>
+            <TableHeadCell>Comprar/vender</TableHeadCell>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Ativo</TableCell>
+              <TableCell>Cotação</TableCell>
+              <TableCell>Quantidade</TableCell>
+              <TableCell>
+                <Button color="light">Comprar/vender</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
---
+++ next-frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import flowbite from "flowbite-react/tailwind";
+//@ts-expect-error - flowbite-typography has no type
+import flowbiteTypography from "flowbite-typography";
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [flowbite.plugin(), flowbiteTypography],
+} satisfies Config;

5600b27e01023f1b710b93069947f26b321ad579
Bruna Nunes <brugnunes@gmail.com>
2025-03-06 18:53:23 -0300

    feat: basic integration with nestjs api list wallets

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -1,6 +1,20 @@
+import { Wallet } from "@/models";
 import { Button, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 
-export default function MyWalleList() {
+export async function getMyWallet(walletId: string): Promise<Wallet> {
+  const response = fetch(`http://localhost:3000/wallets/${walletId}`)
+  return (await response).json();
+}
+
+export default async function MyWalleList({
+  searchParams
+}: {
+  searchParams: Promise<{ wallet_id: string}>;
+}) {
+  // como nao temos autenticacao, passaremos o walletIs via query param
+  const { wallet_id } = await searchParams;
+  const wallet = await getMyWallet(wallet_id)
+
   return (
     <div className="flex flex-col space-y-5 flex-grow">
       <article className="format">
@@ -15,14 +29,17 @@ export default function MyWalleList() {
             <TableHeadCell>Comprar/vender</TableHeadCell>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>Ativo</TableCell>
-              <TableCell>Cotação</TableCell>
-              <TableCell>Quantidade</TableCell>
+            {wallet.assets.map((walletAsset, key) => (
+              <TableRow key={key}>
+              <TableCell>{walletAsset.asset.name}</TableCell>
+              <TableCell>R$ {walletAsset.asset.price}</TableCell>
+              <TableCell>{walletAsset.shares}</TableCell>
               <TableCell>
                 <Button color="light">Comprar/vender</Button>
               </TableCell>
             </TableRow>
+            ))}
+
           </TableBody>
         </Table>
       </div>

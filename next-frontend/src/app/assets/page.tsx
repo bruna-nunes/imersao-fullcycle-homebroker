62dd141f480ccfb1c72d34383988542188b9c1db
Bruna Nunes <brugnunes@gmail.com>
2025-03-06 20:41:16 -0300

    feat: create assets list page, improve wallet list page with asset show component

--- next-frontend/src/app/assets/page.tsx
+++ next-frontend/src/app/assets/page.tsx
@@ -1,42 +1,40 @@
 import { AssetShow } from "@/components/AssetShow";
-import { Wallet } from "@/models";
+import { Asset } from "@/models";
 import { Button, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 
-export async function getMyWallet(walletId: string): Promise<Wallet> {
-  const response = fetch(`http://localhost:3000/wallets/${walletId}`)
-  return (await response).json();
+export async function getAssets(): Promise<Asset[]> {
+  const response = await fetch("http://localhost:3000/assets");
+  return response.json();
 }
 
-export default async function MyWalleListPage({
+export default async function AssetsListPage({
   searchParams
 }: {
   searchParams: Promise<{ wallet_id: string}>;
 }) {
   // como nao temos autenticacao, passaremos o walletIs via query param
   const { wallet_id } = await searchParams;
-  const wallet = await getMyWallet(wallet_id)
+  const assets = await getAssets();
 
   return (
     <div className="flex flex-col space-y-5 flex-grow">
       <article className="format">
-        <h1>Minha carteira</h1>
+        <h1>Ativos</h1>
       </article>
       <div className="overflow-x-auto w-full">
         <Table className="w-full max-w-full table-fixed">
           <TableHead>
             <TableHeadCell>Ativo</TableHeadCell>
             <TableHeadCell>Cotação</TableHeadCell>
-            <TableHeadCell>Quantidade</TableHeadCell>
             <TableHeadCell>Comprar/vender</TableHeadCell>
           </TableHead>
           <TableBody>
-            {wallet.assets.map((walletAsset, key) => (
+            {assets.map((asset, key) => (
               <TableRow key={key}>
               <TableCell>
-                <AssetShow asset={walletAsset.asset}/>
+                <AssetShow asset={asset}/>
               </TableCell>
-              <TableCell>R$ {walletAsset.asset.price}</TableCell>
-              <TableCell>{walletAsset.shares}</TableCell>
+              <TableCell>R$ {asset.price}</TableCell>
               <TableCell>
                 <Button color="light">Comprar/vender</Button>
               </TableCell>
